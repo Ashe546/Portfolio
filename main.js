c2521a3ea2282bc2b7b760902ded092cfa2eed55
Ashe546 <54021207+Ashe546@users.noreply.github.com>
2022-05-16 13:58:17 -0300

    Fixed code review suggestions

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.internal-link');
-// const main = document.querySelector('.cover');
+
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
-  // if (main.classList.contains('active')) {
-  //   main.classList.remove('active');
-  // } else {
-  //   main.classList.toggle('active');
-  // }
 });
 
-document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
