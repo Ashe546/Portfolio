2ab027eb878ba6e51a0caffe8fe22a7964acc2d8
Sajad Rajaee <sajjadrajaee@gmail.com>
2022-05-20 18:19:10 +0430

    Add local storage stripts

--- main.js
+++ main.js
@@ -16,3 +16,50 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+//Local storage
+const inputFields = document.querySelectorAll('input');
+inputFields.forEach((input) => {
+  input.addEventListener('change', (event) => {
+    let formData = JSON.parse(localStorage.getItem('form'));
+    if (!formData) {
+      formData = { name: '', email: '', message: '' };
+    }
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem('formData', JSON.stringify(formData));
+  });
+});
+
+const textArea = document.getElementById('message');
+textArea.addEventListener('change', (event) => {
+  let formData = JSON.parse(localStorage.getItem('form'));
+  if (!formData) {
+    formData = { name: '', email: '', message: '' };
+  }
+  formData.message = event.target.value;
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
+
+function retrieveFormData() {
+  const formData = JSON.parse(localStorage.getItem('formData'));
+  if (formData) {
+    document.getElementById('email').value = formData.email;
+    document.getElementById('name').value = formData.name;
+    document.getElementById('message').value = formData.message;
+  }
+}
+
+retrieveFormData();
+
+// Form Validation
+const form = document.querySelector('.form');
+form.addEventListener('submit', (event) => {
+  const emailInput = form.email.value;
+  if (emailInput.toLowerCase() !== emailInput) {
+    event.preventDefault();
+    const errorTag = document.querySelector('.error-message');
+    errorTag.innerHTML = 'Error: Please insert email in lowercase!';
+  } else {
+    form.action = 'https://formspree.io/f/moqrpbja';
+  }
+});
