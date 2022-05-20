737fe2ee29c2a23272c6b456f1dacc14e5bb80c0
Ashe546 <54021207+Ashe546@users.noreply.github.com>
2022-05-20 13:45:38 -0300

    Made some change to fix linter error and added some style to the error message

--- main.js
+++ main.js
@@ -11,41 +11,8 @@ document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click'
   navMenu.classList.remove('active');
 }));
 
-//Local storage
-const inputFields = document.querySelectorAll('input');
-inputFields.forEach((input) => {
-  input.addEventListener('change', (event) => {
-    let formData = JSON.parse(localStorage.getItem('form'));
-    if (!formData) {
-      formData = { name: '', email: '', message: '' };
-    }
-    formData[event.target.name] = event.target.value;
-    localStorage.setItem('formData', JSON.stringify(formData));
-  });
-});
-
-const textArea = document.getElementById('message');
-textArea.addEventListener('change', (event) => {
-  let formData = JSON.parse(localStorage.getItem('form'));
-  if (!formData) {
-    formData = { name: '', email: '', message: '' };
-  }
-  formData.message = event.target.value;
-  localStorage.setItem('formData', JSON.stringify(formData));
-});
-
-function retrieveFormData() {
-  const formData = JSON.parse(localStorage.getItem('formData'));
-  if (formData) {
-    document.getElementById('email').value = formData.email;
-    document.getElementById('name').value = formData.name;
-    document.getElementById('message').value = formData.message;
-  }
-}
-
-retrieveFormData();
-
 // Form Validation
+
 const form = document.querySelector('.form');
 form.addEventListener('submit', (event) => {
   const emailInput = form.email.value;
@@ -59,6 +26,7 @@ form.addEventListener('submit', (event) => {
 });
 
 // popup
+
 const projects = [
   {
     title: 'Tonic',
