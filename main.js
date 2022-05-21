b75eb986da49a9808aac55e9a7de286e86881187
Ashe546 <asfyd42@gmail.com>
2022-05-21 10:09:37 -0800

    finished the data store project

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.internal-link');
+const navMenu = document.querySelector('.nav-menu');
+const main = document.querySelector('.cover');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
+  if (main.classList.contains('active')) {
+    main.classList.remove('active');
+  } else {
+    main.classList.toggle('active');
+  }
 });
 
-document.querySelectorAll('.nav-item').forEach((n) => n.addEventListener('click', () => {
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
 
+// Form Validation
 const form = document.querySelector('.form');
 form.addEventListener('submit', (event) => {
   const emailInput = form.email.value;
@@ -19,16 +26,25 @@ form.addEventListener('submit', (event) => {
     const errorTag = document.querySelector('.error-message');
     errorTag.innerHTML = 'Error: Please insert email in lowercase!';
   } else {
-    form.action = 'https://formspree.io/f/moqrpbja';
+    form.action = 'https://formspree.io/f/mbjwpgvg';
   }
 });
 
-const projects = [
+// popup section
+const btn = document.querySelector('.button');
+const popupactive = document.querySelector('.popup');
+const popupConstainer = document.querySelector('.popup-container');
 
+btn.addEventListener('click', () => {
+  popupactive.classList.toggle('active');
+  popupConstainer.classList.toggle('show');
+});
+
+const projects = [
   {
     title: 'Tonic',
     roles: { company: 'Canopy', position: ['Back End Dev', '2015'] },
-    image: '/image/Snapshoot-Portfolio.png',
+    image: 'images/SnapshootPortfolioD1.png',
     alt: 'Canopy project screenshot',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     skills: ['html', 'css', 'javascript'],
@@ -37,13 +53,13 @@ const projects = [
     seesource: { link: 'http://github.com/felix45/portfolio', text: 'See Source' },
     seeproject: 'See Project',
     htmlClass: { article: ['grid-item', 'card'], cardImage: ['card-image'], cardDescription: ['card-description'] },
-    cssStyle: '',
+    cssStyle: 'cardContainer',
   },
 
   {
     title: 'Multi-Post Stories',
     roles: { company: 'Facebook', position: ['Full Stack Dev', '2015'] },
-    image: '/image/Snapshoot-Portfolio(3).png',
+    image: 'images/SnapshootPortfolio.png',
     alt: 'Multi-Post stories project screenshot',
     description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
     skills: ['html', 'Ruby on rails', 'css', 'javascript'],
@@ -52,13 +68,13 @@ const projects = [
     seesource: { link: 'https://github.com/felix45/portfolio', text: 'See Source' },
     seeproject: 'See Project',
     htmlClass: { article: ['grid-item', 'card', 'hide-ruby'], cardImage: ['card-image', 'flex-item-2'], cardDescription: ['card-description', 'flex-item-1'] },
-    cssStyle: 'card-l',
+    cssStyle: 'cardContainerRight',
   },
 
   {
     title: 'Facebook 360',
     roles: { company: 'Facebook', position: ['Full Stack Dev', '2015'] },
-    image: '/image/Snapshoot-Portfolio(4).png',
+    image: 'images/SnapshootPortfolio2.png',
     alt: 'Facebook 360 project screenshot',
     description: 'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
     skills: ['html', 'Ruby on rails', 'css', 'javascript'],
@@ -67,13 +83,13 @@ const projects = [
     seesource: { link: 'https://github.com/felix45/portfolio', text: 'See Source' },
     seeproject: 'See Project',
     htmlClass: { article: ['grid-item', 'card', 'flex-item-2', 'hide-ruby'], cardImage: ['card-image'], cardDescription: ['card-description'] },
-    cssStyle: '',
+    cssStyle: 'cardContainer',
   },
 
   {
     title: 'Uber Navigation',
     roles: { company: 'Uber', position: ['Lead Developer', '2018'] },
-    image: '/image/Snapshoot-Portfolio(5).png',
+    image: 'images/SnapshootPortfolio3-1.png',
     alt: 'Uber Navigation project screenshot',
     description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car..',
     skills: ['html', 'Ruby on rails', 'css', 'javascript'],
@@ -82,8 +98,8 @@ const projects = [
     seesource: { link: 'http://github.com/felix45/portfolio', text: 'See Source' },
     seeproject: 'See Project',
     htmlClass: { article: ['grid-item', 'card', 'flex-item-3', 'flex-item-last', 'hide-ruby'], cardImage: ['card-image', 'flex-item-2'], cardDescription: ['card-description', 'flex-item-1'] },
-    cssStyle: 'card-l',
-
+    cssStyle: 'cardContainerRight',
+    colse: 'close',
   },
 ];
 
@@ -91,80 +107,77 @@ const section = document.getElementById('portfolio');
 projects.forEach((project) => {
   const div = document.createElement('div');
   div.className = 'main-container';
-  div.innerHTML = ` <div class="${project.cssStyle} card"> 
-<div class="res-snapshoot">
- <img class="Snapshoot" src= ${project.image}> 
- </div>
-  <div class="res-frame">
-      <h1 class="title">${project.title}</h1> 
-      <ul class="frame">
-      <li class="detail-1">CANOPY</li>
-      <li class="detail">BACK END DEV</li>
-      <li class="detail">2015</li>
-      </ul>
-      <p class="detailp">${project.description}</p>
-      <div class="tags"> 
-      <button class="tag">html</button> 
-      <button class="tag">css</button>
-      <button class="tag">javascript</button>
+  div.innerHTML = `     <article class="left">
+      <div class=${project.cssStyle}>
+        <img class="img" src= ${project.image} alt="First Image" />
+        <div class="detail">
+          <h3>${project.title}</h3>
+          <h4>
+            <span>CANOPY</span> &nbsp;
+            <img src="images/Counter.png" alt="counter" />&nbsp; Back End Dev
+            &nbsp; <img src="images/Counter.png" alt="counter" />&nbsp; 2015
+          </h4>
+          <p>
+            ${project.description}
+          </p>
+          <ul>
+            <li>html</li>
+            <li>CSS</li>
+            <li>javascript</li>
+          </ul>
+          <button class="button" value = ${projects.indexOf(project)}> ${project.seeproject}</button>
+        </div>
       </div>
-       <button type="button" value = ${projects.indexOf(project)} class="normal-button">${project.seeproject}</button> 
-     </div>
-</div>`;
+    </article>
+
+    <article class="card">
+        <div class="cardContainer">
+          <img class="img" src = ${project.image} alt="First Image" />
+          <h3>${project.title}</h3>
+          <h4>
+            <span>CANOPY</span>&nbsp;
+            <img src="images/Counter.png" alt="counter" />&nbsp; Back End
+            Dev&nbsp; <img src="images/Counter.png" alt="counter" />&nbsp; 2015
+          </h4>
+          <p>
+              ${project.description}
+          </p>
+          <ul>
+            <li>html</li>
+            <li>CSS</li>
+            <li>javascript</li>
+          </ul>
+          <button class="button"  value = ${projects.indexOf(project)}> ${project.seeproject}</button>
+        </div>
+      </article>`;
   section.appendChild(div);
 });
 
 const modal = document.getElementById('myModal');
-projects.forEach((project) => {
-  document.querySelectorAll('.normal-button').forEach((n) => n.addEventListener('click', () => {
+const btn1 = document.querySelectorAll('.button');
+
+const titlePopup = document.querySelector('.popup-title');
+const imagePopup = document.querySelector('.back-image');
+
+btn1.forEach((btn) => {
+  btn.addEventListener('click', function () {
     modal.style.display = 'block';
-    modal.innerHTML = ` <div class="popup-card">
-<div class = "popup-close">
-        <h1 class = "popup-h1">${project.title}</h1>
-        
-</div>
-        <div class = "popup-frame">
-        <ul class="popup-frame">
-          <li class="detail-1">CANOPY</li>
-          <li class="detail">BACK END DEV</li>
-          <li class="detail">2015</li>
-        </ul>
-        </div>
-        <div class = "popup-img-card" style = "background-image: url('.${project.image}'); max-height: 300px; "></div>
-<div class = "popup-description">
-<div class = "desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, </div>
-<div class = "pop-frame">  
- <div class="tags">
-<button class="tag">html</button>
-<button class="tag">css</button>
-<button class="tag">javascript</button>
-</div>
-<div class="tags popup-tag">
-<button class="tag">github</button>
-<button class="tag">ruby</button>
-<button class="tag">Bootstraps</button>
-</div>
-<hr class = "popup-hr">
-<div class = "popup-link-button">
-<button class = "popup-button">See More <img src = "./image/pop.svg"></button>
-<button class = "popup-button">See More <img src = "./image/Icon-GitHub.svg"></button>
-</div>
-</div>
-      </div> `;
-  }));
+    const x = this.value;
+    console.log(this.value[0]);
+    titlePopup.innerHTML = `<h2 class="popup-h">${projects[x].title}</h2>`;
+    imagePopup.innerHTML = `<div class = "popup-img-card" style = "background-image: url('${projects[x].image}');"></div>
+  `;
+  });
 });
 
-window.onclick = function (event) {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-};
+const closeIcon = document.querySelector('.closeicon');
+closeIcon.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
 
+// Local Storage
 const inputFields = document.querySelectorAll('input');
+console.log(inputFields)
 inputFields.forEach((input) => {
   input.addEventListener('change', (event) => {
     let formData = JSON.parse(localStorage.getItem('formData'));
